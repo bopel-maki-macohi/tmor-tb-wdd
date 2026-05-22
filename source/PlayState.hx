fc5cb0803fc8cfc5546749049f84b564186bb788
bopel-maki-macohi <bopel.maki.macohi@gmail.com>
2026-05-21 21:15:02 -0500

    BUllets

--- source/PlayState.hx
+++ source/PlayState.hx
@@ -3,6 +3,7 @@ package;
 import flixel.FlxG;
 import flixel.FlxSprite;
 import flixel.FlxState;
+import flixel.group.FlxSpriteGroup;
 import flixel.util.FlxColor;
 
 class PlayState extends FlxState
@@ -10,9 +11,14 @@ class PlayState extends FlxState
 	var player:FlxSprite;
 	var playerScreenOffset:Float = 0;
 
+	var bullets:FlxSpriteGroup;
+
 	override public function create()
 	{
 		super.create();
+
+		bullets = new FlxSpriteGroup();
+		add(bullets);
 
 		player = new FlxSprite().makeGraphic(128, 128, FlxColor.RED);
 		add(player);
@@ -37,5 +43,23 @@ class PlayState extends FlxState
 			player.x = playerScreenOffset;
 		if (player.x > FlxG.width - player.width - playerScreenOffset)
 			player.x = FlxG.width - player.width - playerScreenOffset;
+
+		if (FlxG.keys.justPressed.SPACE)
+		{
+			var bullet = new FlxSprite().makeGraphic(8, 16, FlxColor.YELLOW);
+			bullet.setPosition(player.getGraphicMidpoint().x, player.getGraphicMidpoint().y);
+			bullets.add(bullet);
+		}
+
+		for (bullet in bullets)
+		{
+			bullet.y -= bullet.height * .5;
+
+			if (bullet.y < -bullet.height)
+			{
+				bullets.remove(bullet);
+				bullet.destroy();
+			}
+		}
 	}
 }
